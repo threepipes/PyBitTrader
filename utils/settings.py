b0444b60a5350e551f875ae6710da4e22774bb23
Shogo Tsutsumi <threepipesatos@gmail.com>
2018-01-18 19:11:43 +0900

    [fix]本番環境用のセッティング追加(ログ出力)

--- utils/settings.py
+++ utils/settings.py
@@ -1,11 +1,18 @@
+import os
 from logging import getLogger, basicConfig, DEBUG
 
 app_name = 'pybittrader'
+env = os.getenv('PYBITTRADER_ENV', 'debug')
 
 
 def logging_config():
-    basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
-                datefmt='%Y-%m-%d %H:%M:%S')
+    if env == 'debug':
+        basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+    else:
+        basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    filename='log.txt')
     app_logger = getLogger(app_name)
     app_logger.setLevel(DEBUG)
 
