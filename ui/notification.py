2e960fdba3efcf458163d79b3654e713d1a1c345
Shogo Tsutsumi <threepipesatos@gmail.com>
2018-01-28 11:49:26 +0900

    [fix]slack送信先デフォルトを変更

--- ui/notification.py
+++ ui/notification.py
@@ -5,6 +5,6 @@ import slackweb
 WEBHOOK_URL = os.environ.get('MY_INC_URL', '')
 
 
-def slack(message, channel='#sys-log'):
+def slack(message, channel='#bittrader'):
     sl = slackweb.Slack(url=WEBHOOK_URL)
     sl.notify(text=message, channel=channel)
