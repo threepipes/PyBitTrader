4ef580e6428044fa725bb2ad1f036392bae15697
Shogo Tsutsumi <s-tutumi@ist.osaka-u.ac.jp>
2018-02-08 09:21:19 +0900

    [fix]slack通知失敗時に落ちるのを修正

--- ui/notification.py
+++ ui/notification.py
@@ -1,10 +1,17 @@
 # coding: UTF-8
 import os
 import slackweb
+from urllib.error import HTTPError
+from utils.settings import get_logger
 
+logger = get_logger()
 WEBHOOK_URL = os.environ.get('MY_INC_URL', '')
 
 
 def slack(message, channel='#bittrader'):
     sl = slackweb.Slack(url=WEBHOOK_URL)
-    sl.notify(text=message, channel=channel)
+    try:
+        sl.notify(text=message, channel=channel)
+    except HTTPError as e:
+        logger.error('slack error')
+        logger.exception(e)
