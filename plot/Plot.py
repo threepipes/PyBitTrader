463720da1b0fc10a1455f1bd14c484115016c6e3
Shogo Tsutsumi <threepipesatos@gmail.com>
2018-02-09 17:56:24 +0900

    [fix]dfへの変換を修正

--- plot/Plot.py
+++ plot/Plot.py
@@ -7,7 +7,7 @@ def plot_recent_order(cc_order):
     resource = cc_order.price * cc_order['size']
     sr_res = resource / resource[0]
     sr_gen = cc_order.price / cc_order.price[0]
-    plot_df = pd.DataFrame([sr_res, sr_gen], columns=['resource', 'price'])
+    plot_df = pd.DataFrame([sr_res, sr_gen], index=['resource', 'price']).T
     fig = plot_df.plot(figsize=(15, 5)).get_figure()
     html = mpld3.fig_to_html(fig)
     plt.clf()
